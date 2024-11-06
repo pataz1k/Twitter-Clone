885d9480aa19245d738ae8be9737b4884a172068
pataz1k <alekseiloker1@gmail.com>
2024-11-06 22:13:03 +0500

    ---

--- frontend/src/components/ui/CreatePost/CreatePost.tsx
+++ frontend/src/components/ui/CreatePost/CreatePost.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames'
 import { FC, useContext, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { useQuery } from 'react-query'
 
 import { AuthContext } from '@/providers/AuthProvider'
@@ -17,10 +18,12 @@ interface IProfileResponse {
 	data: IProfile
 }
 
-const CreatePost: FC = () => {
-	const [inputValue, setInputValue] = useState('')
-	const [lettersCount, setLettersCount] = useState(0)
+type Inputs = {
+	caption: string
+}
 
+const CreatePost: FC = () => {
+	const [lettersCount, setLettersCount] = useState(0)
 	const { isAuth, accessToken } = useContext(AuthContext)
 
 	const { isSuccess, data } = useQuery(
@@ -29,62 +32,73 @@ const CreatePost: FC = () => {
 		{ select: ({ data }: { data: IProfileResponse }) => data, enabled: isAuth }
 	)
 
-	const handleChange = (event) => {
-		setLettersCount(event.target.value.length)
-		setInputValue(event.target.value)
-		adjustHeight(event.target)
-	}
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>()
+	const onSubmit = (data) => console.log(data)
 
 	const adjustHeight = (element) => {
 		element.style.height = 'auto'
 		element.style.height = `${element.scrollHeight}px`
 	}
+
 	return (
 		<div className={classes.wrap}>
-			<ProfileItem
-				avatar={data?.data.avatar!}
-				username={data?.data.username!}
-				id={data?.data._id!}
-			/>
-			<textarea
-				name="create post"
-				className={classes.input}
-				draggable="false"
-				placeholder="What's happening?"
-				value={inputValue}
-				onChange={handleChange}
-				maxLength={240}
-			/>
-			<div className={classes.info}>
-				<div className={classes.additionalButtons}>
-					<button>
-						<MaterialIcon name="MdOutlineImage" />
-					</button>
-					<button>
-						<MaterialIcon name="MdOutlineGifBox" />
-					</button>
-					<button>
-						<MaterialIcon name="MdOutlineEmojiEmotions" />
-					</button>
-					<button>
-						<MaterialIcon name="MdOutlineLocationOn" />
-					</button>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<ProfileItem
+					avatar={data?.data.avatar!}
+					username={data?.data.username!}
+					id={data?.data._id!}
+				/>
+				<textarea
+					className={classes.input}
+					draggable="false"
+					placeholder="What's happening?"
+					maxLength={240}
+					{...register('caption', {
+						required: { value: true, message: 'Caption is required' },
+						onChange: (event) => {
+							setLettersCount(event.target.value.length)
+							adjustHeight(event.target)
+						},
+					})}
+				/>
+				<div className={classes.info}>
+					<div className={classes.additionalButtons}>
+						<button>
+							<MaterialIcon name="MdOutlineImage" />
+						</button>
+						<button>
+							<MaterialIcon name="MdOutlineGifBox" />
+						</button>
+						<button>
+							<MaterialIcon name="MdOutlineEmojiEmotions" />
+						</button>
+						<button>
+							<MaterialIcon name="MdOutlineLocationOn" />
+						</button>
+					</div>
+					<div className="flex gap-3 items-center">
+						<p
+							className={cn('p-2 px-4 rounded-2xl transition-colors', {
+								'bg-blue-500': lettersCount <= 180,
+								'bg-red-500': lettersCount === 240,
+								'bg-orange-500': lettersCount >= 180,
+							})}
+						>
+							{lettersCount} / 240
+						</p>
+						<button
+							type="submit"
+							className="bg-blue-500 p-2 px-4 rounded-2xl hover:bg-blue-700 transition-colors"
+						>
+							Tweet
+						</button>
+					</div>
 				</div>
-				<div className="flex gap-3 items-center">
-					<p
-						className={cn('p-2 px-4 rounded-2xl transition-colors', {
-							'bg-blue-500': lettersCount <= 180,
-							'bg-red-500': lettersCount === 240,
-							'bg-orange-500': lettersCount >= 180,
-						})}
-					>
-						{lettersCount} / 240
-					</p>
-					<button className="bg-blue-500 p-2 px-4 rounded-2xl hover:bg-blue-700 transition-colors">
-						Tweet
-					</button>
-				</div>
-			</div>
+			</form>
 		</div>
 	)
 }
