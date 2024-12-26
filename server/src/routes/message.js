1c36e2263de30aed18f58096fdaae49a136be53a
Pataz1k <86663180+pataz1k@users.noreply.github.com>
2024-12-26 10:21:35 +0500

    ---

--- server/src/routes/message.js
+++ server/src/routes/message.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message');
-const jwt = require('jsonwebtoken');
 const { protect } = require('../middlewares/auth');
 
 // Get all dialogs for the user
